386443ffdb166fff19a1d21f700b2b1f914c8e2b
wilfridasisilia <wilfridasisilia05@gmail.com>
2024-09-05 14:01:34 +0700

    07. If Statements

---
+++ if_statements_7.py
@@ -0,0 +1,34 @@
+# if = Do some code only if some condition is True
+# else do something else
+#
+# age = int(input("Enter your age: "))
+# if age >= 100:
+#     print("You are too old to sign up")
+# elif age >= 18:
+#     print("You are nocw signed up")
+# elif age < 0:
+#     print("You haven't been born yet")
+# else:
+#     print("You must be 18+ to sign up")
+#
+# response = input("Would you like food?: ")
+# if response == 'Y' or response == 'y' or response == 'yes':
+#     print("Have some food.")
+# else:
+#     print("No food for you")
+#
+# ###############################################
+#
+# name = input("Enter your name: ")
+# if name == "":
+#     print("You did not type in your name.")
+# else
+#     print(f"Hello {name}. Nice to know you")
+#
+# ################################################
+
+online = False
+if online:
+    print("The user is online")
+else:
+    print("The user is offline")
