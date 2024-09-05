62dae324db44794e0933164423dace27df3bcc98
wilfridasisilia <wilfridasisilia05@gmail.com>
2024-09-05 20:06:34 +0700

    14. String Indexing

---
+++ string_indexing_14.py
@@ -0,0 +1,19 @@
+# Indexing = accessing elements of a sequence using[] (indexing operator)
+# [start:end:step]
+
+credit_number = "1234-5678-9012-3456"
+# print(credit_number[0])
+# print(credit_number[:4])
+# print(credit_number[5:9])
+# print(credit_number[15:])
+# print(credit_number[-1])
+# print(credit_number[::2]) #step 2
+
+
+# last_digits = credit_number[-4:]
+# print(f"xxxx-xxxx-xxxx-{last_digits}")
+
+credit_number = credit_number[::-1] #membalik semua angka dari belakang
+print(credit_number)
+
+
