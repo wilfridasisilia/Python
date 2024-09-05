3564012438d9c4ff46a304144dad68b5b6b47614
wilfridasisilia <wilfridasisilia05@gmail.com>
2024-09-05 12:42:57 +0700

    04. User Input

---
+++ user_input_4.py
@@ -0,0 +1,8 @@
+# input() = A function that prompts the user to enter data.
+# Returns the entered data as a string
+
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+
+print(f"Hello {name}, Nice to meet you.")
+print(f"You are {age} years old")
