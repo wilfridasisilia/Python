fe53edc67362779b499eb0a7357247b24de94cc8
wilfridasisilia <wilfridasisilia05@gmail.com>
2024-09-05 12:38:42 +0700

    03. TypeCasting

---
+++ type_casting_3.py
@@ -0,0 +1,15 @@
+# TypeCasting = the process of converting a variable from one data type
+# to another. str(), int(), float(), bool()
+
+name = "frida"
+age = 19
+gpa = 4.0
+is_student = True
+
+name = bool(name)
+print(type(name))
+print(name)
+
+print(type(gpa))
+print(int(gpa))
+print(gpa)
