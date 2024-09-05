b359c67354cb351cc37dfdc0ef9a4c4a4fb9d18f
wilfridasisilia <wilfridasisilia05@gmail.com>
2024-09-05 14:13:59 +0700

    08. Calculator Program

---
+++ calculator_program_8.py
@@ -0,0 +1,18 @@
+# Python calculator
+
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+result = 0
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    result = num1 / num2
+else:
+    print(f"{operator} is not valid operator")
+print(round(result,3))
