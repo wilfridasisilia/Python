40476b78585be3bb7f6dc9588c66aa8c686e7c4e
wilfridasisilia <wilfridasisilia05@gmail.com>
2024-09-05 13:02:14 +0700

    05. Madlibs Game

---
+++ madlibs_game_5.py
@@ -0,0 +1,14 @@
+# Madlibs game
+# word game where you create a story
+# by filling in blanks with random words
+
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (persone, place, things: ")
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb ending woth 'ing': ")
+adjective3 = input("Enter an adjective (description): ")
+
+print(f"Today i went to a {adjective1} zoo.")
+print(f"In exhibit, i saw a {noun1}.")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}.")
