2e53a98b1c683c85021c589fa387d5b96f71e4c4
wilfridasisilia <wilfridasisilia05@gmail.com>
2024-09-05 12:33:43 +0700

    02. Variable

---
+++ variable_2.py
@@ -0,0 +1,34 @@
+# Variable = A container for a value (string, integer, float
+# boolean). A variable behaves as if it was the value it contains
+
+# Strings
+first_name = "Frida"
+food = "Pizza"
+email = "frida12@gmail.com"
+
+print(f"Hello, {first_name}")
+print(f"You like {food}")
+print(f"Your email is: {email}")
+# print("Frida")
+
+#Integers
+age = 19
+quantity = 3
+num_of_students = 30
+print(f"You are {age} yo")
+
+# Float
+price = 10.99
+height = 155.9
+gpa = 4.0
+distance = 5.5
+print(f"The Price is ${price}")
+
+#Boolean
+is_student = True
+# for_sale = False
+print(f"Are you a student?: {is_student}")
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
