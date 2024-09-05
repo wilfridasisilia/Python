2e938e06284f3516264b28994ba8ab4325058d7d
wilfridasisilia <wilfridasisilia05@gmail.com>
2024-09-05 13:47:00 +0700

    06. Aritmethic Math

---
+++ aritmethic_math_6.py
@@ -0,0 +1,54 @@
+import math
+# friends = 10
+# friends = friends + 1
+# friends += 1
+# friends = friends - 2
+# friends -= 2
+# friends = friends * 3
+# friends *= 3
+# friends = friends / 2
+# friends /= 2
+# friends = friends ** 2
+# friends **= 2
+
+# remainder = friends % 3
+# print(remainder)
+
+# x = 3.14
+# y = -4
+# z = 5
+
+# result = round(x)
+# result = abs(y)
+# result = pow(4,3) #4x4x4
+# result = max(x, y, z)
+# result = min(x, y, z)
+#
+# print(result)
+
+##############################################
+# x = 9.1
+# print(math.pi)
+# print(math.e)
+# result = math.sqrt(x) #square root
+# result = math.ceil(x)
+# result = math.floor(x)
+#
+# print(result)
+
+################################################
+# radius = float(input("Enter the radius of a circle: "))
+# circumference = 2 * math.pi * radius
+# print(f"The circumference is: {round(circumference,2)}cm")
+
+################################################
+# radius = float(input("Enter the radius of a circle: "))
+# area = math.pi * pow(radius,2)
+# print(f"The area of the circle is: {round(area,2)}cm^2")
+
+#################################################
+a = float(input("Enter side a: "))
+b = float(input("Enter side b: "))
+c = math.sqrt(pow(a,2)+pow(b,2))
+print(f"The side of c is {c}")
+
