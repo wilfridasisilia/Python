f0ea0a4ea46ad96e90affb0f5ea59c190655b75a
wilfridasisilia <wilfridasisilia05@gmail.com>
2024-09-05 20:18:35 +0700

    15. Format Specifiers

---
+++ format_specifiers_15.py
@@ -0,0 +1,20 @@
+# format specifiers = {:flags} format a value based on what flags are inserted
+
+# .(number)f = round to that many decimal places (fixed point)
+# :(number) = allocate that many spaces
+# :03 = allocate and zero pad that many spaces
+# :< = left justify
+# :> = right justify
+# :^ = center align
+# :+ = use a plus sign to indicate positive value
+# := = place sign to leftmost position
+# :  = insert a space before positive numbers
+# :, = comma separator
+
+price1 = 3.14159
+price2 = -987.65
+price3 = 12.34
+
+print(f"Price 1 is ${price1:,}")
+print(f"Price 2 is ${price2}")
+print(f"Price 3 is ${price3}")
