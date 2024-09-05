648e56b609fd986814f0c2898bdb75581df14818
wilfridasisilia <wilfridasisilia05@gmail.com>
2024-09-05 12:52:29 +0700

    Exercise 1 dan 2

---
+++ exercise_1.py
@@ -0,0 +1,9 @@
+# Execirse 1 rectangle area calc
+
+length = float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+area = length * width
+
+print(f"The area is: {area}cm2")
+
+print(area)
