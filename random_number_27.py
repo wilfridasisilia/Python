3685e4c062144e2b0ea1fd2079a1f8c2a58e74bd
wilfridasisilia <wilfridasisilia05@gmail.com>
2024-09-09 11:55:18 +0700

    27. Random Number

---
+++ random_number_27.py
@@ -0,0 +1,10 @@
+import random
+low = 1
+high = 100
+options = ('rock', 'paper', 'scissors')
+cards = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
+# option = random.choice(options)
+# number = random.randint(low, high)
+# number = random.random()
+card = random.shuffle(cards)
+print(cards)
